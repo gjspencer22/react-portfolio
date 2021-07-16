8d8af078905933f03dc761e3e91e65ad8a1d3a76
gjspencer22 <gjspencer22@gmail.com>
2021-07-16 00:54:22 -0600

    updated css some

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-function Contact(props) {
+import React from 'react'; 
+import './index.css'
+function Contact() {
     return (
         <div className="feedback">
             <div>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import './index.css'
 function Footer() {
     return (
-        <div className="footerBackground">
-            <h3 className="footer">Contact <h5>Email: gjspencer22@gmail.com</h5></h3>
-            <div className="footerInfo">
-                
-            </div>
+        <div className="footer">
+            <h3>Contact:</h3>
+            <h3>Email: gjspencer22@gmail.com</h3>
+            <h3> Github</h3>
         </div>
     );
 }
